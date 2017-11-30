6683507d5fb9f8bbad6b14a5cccfcd13abfa4e80
She-Wolf <shewolf664050@gmail.com>
2017-11-30 19:34:59 +0300

    Исправлен баг красивости ()

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -9,14 +9,14 @@ def get_roots(a, b, c):
         root2 = 0
         root1 = (-b - sqrt(discriminant)) / (2 * a)
         root2 = (-b + sqrt(discriminant)) / (2 * a)
-        return (root1, root2)
+        return root1, root2
     elif discriminant == 0:
         root1 =0
         root1 =  ((-b - sqrt(discriminant)) / (2 * a))
-        return (root1, None)
+        return root1, None
     elif discriminant < 0:
         root1 = 0
         root2 = 0
         root1 = (-b - cmath.sqrt(discriminant)) / (2 * a)
         root2 = (-b + cmath.sqrt(discriminant)) / (2 * a)
-        return (None, None)
+        return None, None
