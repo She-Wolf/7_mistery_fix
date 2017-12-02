b4f01d6572547959182424b175cdc572ffbe73c3
She-Wolf <shewolf664050@gmail.com>
2017-12-02 20:27:48 +0300

    Подчесала код

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,22 +1,13 @@
-from math import sqrt
-import cmath
+from cmath import sqrt
 
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
+    root1 = (-b - sqrt(discriminant)) / (2 * a)
+    root2 = (-b + sqrt(discriminant)) / (2 * a)
     if discriminant > 0:
-        root1 = 0
-        root2 = 0
-        root1 = (-b - sqrt(discriminant)) / (2 * a)
-        root2 = (-b + sqrt(discriminant)) / (2 * a)
         return root1, root2
     elif discriminant == 0:
-        root1 =0
-        root1 =  ((-b - sqrt(discriminant)) / (2 * a))
         return root1, None
     elif discriminant < 0:
-        root1 = 0
-        root2 = 0
-        root1 = (-b - cmath.sqrt(discriminant)) / (2 * a)
-        root2 = (-b + cmath.sqrt(discriminant)) / (2 * a)
         return None, None
